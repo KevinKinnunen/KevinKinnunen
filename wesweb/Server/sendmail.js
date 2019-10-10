261dc14c9a36b59f0a754b49ba610b9f110d5944
KevinKinnunen <kevin.schwetzkinnunen@elev.ga.ntig.se>
2019-10-10 11:10:59 +0200

    step 1-2

---
+++ wesweb/Server/sendmail.js
@@ -0,0 +1,27 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'kevin.schwetzkinnunen@elev.ga.ntig.se',
+        pass: ''
+    }
+});
+
+var mailOptions = {
+    from: 'kevin.schwetzkinnunen@elev.ga.ntig.se',
+    to: 'niklas.hjelm@ga.ntig.se',
+    subject: 'Skickar Email Med Node.js',
+    text: 'Hej Niklas! //Skickat ifrån Node.js'
+};
+
+exports.mail = function () {
+    
+ transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
+}
