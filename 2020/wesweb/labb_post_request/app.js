3826597a6ef048cfac91e7ecccd1aa0d3c914456
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2020-09-24 15:07:57 +0200

    aweaw

---
+++ 2020/wesweb/labb_post_request/app.js
@@ -0,0 +1,24 @@
+// npm init -y
+// npm i express mongoose
+// npm i --save-dev nodemon
+
+const express = require('express')
+const app = express()
+const port = 3000
+const clientDir = __dirname + '\\client\\'
+
+const bodyParser = require('body-parser')
+const { response } = require('express')
+
+app.use(bodyParser())
+
+app.get('/', (req, res) => res.sendFile(clientDir + 'index.html'))
+app.get('/style', (req, res) => res.sendFile(clientDir + 'style.css'))
+app.get('/apa.jpg', (req, res) => res.sendFile(clientDir + 'apa.jpg'))
+
+app.post('/', function(req, res) {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
