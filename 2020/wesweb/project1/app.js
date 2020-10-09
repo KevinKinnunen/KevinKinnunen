e308203db450771db22cacc3197e5c2f4486316a
KevinKinnunen <kevin.schwetzkinnunen@elev.ga.ntig.se>
2020-10-09 12:26:10 +0200

    w

--- 2020/wesweb/project1/app.js
+++ 2020/wesweb/project1/app.js
@@ -8,6 +8,8 @@ const databaseModule = require('./databaseModule')
 const app = express()
 const port = 3000
 
+const personModule = require('./personModule')
+
 const clientDir = __dirname + "\\client\\"
 
 app.use(express.json())
@@ -28,8 +30,8 @@ app.get('/style', (req, res) => {
 app.post('/', (req, res) => {
   console.log(req.body.name)
   console.log(req.body.email)
-
-  databaseModule.storePerson(req.body.name, req.body.email, req.body.age)
+  let person = personModule.newPerson(req.body.name, req.body.email, req.body.age)
+  databaseModule.storeModel(person)
 
   res.render("pages/index.ejs", {name: " "+req.body.name})
 })
