b579a821c7636876ec0c9e5836a2eb16fe362ae9
KevinKinnunen <kevin.schwetzkinnunen@elev.ga.ntig.se>
2020-10-13 09:30:56 +0200

    Create fizzbuzz.js

---
+++ 2020/wesweb/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,15 @@
+for (let index = 1; index < 100; index++) {
+    if(index % 2==0){
+        console.log(index)
+    }
+    else if(index % 3==0 && index % 5 ==0){
+        console.log("FizzBuzz")
+    }
+    else if(index % 3 ==0){
+        console.log("Fizz")
+    }
+    else if(index % 5==0){
+        console.log("Buzz")
+    }
+}
+
